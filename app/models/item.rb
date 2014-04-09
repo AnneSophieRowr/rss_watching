439f2703137860706910298206f0f181395b20ff
annesophie <annesophie@annesophie-alien.(none)>
2014-04-09 15:22:33 +0200

    duplicates + checking dead links

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,16 @@ class Item < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :title, :link, :description, :date, :user_id
+
+  validates_uniqueness_of :link
+
+  validate :dead_link
+
+  require 'net/http'
+  def dead_link
+    url = URI.parse(link)
+    req = Net::HTTP.new(url.host, url.port)
+    res = req.request_head(url.path)
+    errors.add(:link, :dead) unless res.code == '200'
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module RssWatching
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
+
+    RssWatching::Application.config.secret_key_base = 'bbdf4463ef30e82e988d23c3a5799acfdcc2576130b4de6aeb63a22975da23b2c4a9e636029641bd000ee3293742aeb9a62511c6c4509dd8f16916a64425a65e'
   end
 end
