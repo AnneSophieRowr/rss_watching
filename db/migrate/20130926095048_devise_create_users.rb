953a1c2c0a52ea836cfe2fd19e96a67a61e3e428
annesophie <annesophie@annesophie-alien.(none)>
2014-07-01 15:55:22 +0200

    fix logout

--- db/migrate/20130926095048_devise_create_users.rb
+++ db/migrate/20130926095048_devise_create_users.rb
@@ -8,6 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :first_name,					:null => false
 
       t.integer  :sign_in_count, :default => 0, :null => false
+      t.datetime :remember_created_at
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at
       t.string   :current_sign_in_ip
