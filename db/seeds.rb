f05122c3bde92bbb408f965406fc1cf298f5813c
annesophie <annesophie@annesophie-alien.(none)>
2014-07-03 11:45:45 +0200

    add users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,18 @@ User.delete_all
 User.create(	[
   {email: 'laurent.hugou@bpi.fr', password: 'password', last_name: 'Hugou', first_name: 'laurent'},
   {email: 'anne-sophie.rouaux@bpi.fr', password: 'password', last_name: 'rouaux', first_name: 'anne-Sophie'},
-  {email: 'silvere.mercier@bpi.fr', password: 'password', last_name: 'mercier', first_name: 'silvere'}
+  {email: 'silvere.mercier@bpi.fr', password: 'password', last_name: 'mercier', first_name: 'silvere'},
+  {email: 'anne.gourhand@bpi.fr', password: 'password', last_name: 'gourhand', first_name: 'anne'},
+  {email: 'ali.chihani@bpi.fr', password: 'password', last_name: 'chihani', first_name: 'ali'},
+  {email: 'claudine.villette@bpi.fr', password: 'password', last_name: 'villette', first_name: 'claudie'},
+  {email: 'Abdelkader.belkhiter@bpi.fr', password: 'password', last_name: 'BELKHITER', first_name: 'Abdelkader'},
+  {email: 'yann.faure@bpi.fr', password: 'password', last_name: 'faure', first_name: 'yann'},
+  {email: 'fabienne.charraire@bpi.fr', password: 'password', last_name: 'charraire', first_name: 'fabienne'},
+  {email: 'veronique.abot@bpi.fr', password: 'password', last_name: 'abot', first_name: 'véronique'},
+  {email: 'catherine.brodard@bpi.fr', password: 'password', last_name: 'brodard', first_name: 'catherine'},
+  {email: 'delphine.nicolas@bpi.fr', password: 'password', last_name: 'nicolas', first_name: 'delphine'},
+  {email: 'marie-helene.gatto@bpi.fr', password: 'password', last_name: 'gatto', first_name: 'marie-helene'},
+  {email: 'nathalie.nosny@bpi.fr', password: 'password', last_name: 'nosny', first_name: 'nathalie'},
+  {email: 'natalie.nyang@bpi.fr', password: 'password', last_name: 'niang', first_name: 'natalie'}
   ]
 )
